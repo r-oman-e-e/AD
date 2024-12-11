3b78c30fa3b893a138868a8b0895cb74934cb5ad
r-oman-e-e <romane.froger@free.fr>
2024-12-11 12:18:57 +0100

    maj code

--- code API.R
+++ code API.R
@@ -24,8 +24,13 @@ all_cocktails <- bind_rows(all_cocktails)
 all_cocktails = as.data.frame(all_cocktails)
 
 all_cocktails = all_cocktails %>% 
-  select(-idDrink, -strDrinkAlternate, -strTags, -strVideo, -strIBA, -strInstructionsDE, -strInstructionsES, -strInstructionsFR, -strInstructionsIT, -`strInstructionsZH-HANS` , -`strInstructionsZH-HANT`, -strDrinkThumb, -strIngredient12, -strIngredient13, -strIngredient14, -strIngredient15, -strImageSource, -strImageAttribution, -strCreativeCommonsConfirmed, -dateModified) %>% 
-  rename(name = strDrink,
+  select(-idDrink, -strDrinkAlternate, -strTags, -strVideo, -strIBA, 
+         -strInstructionsDE, -strInstructionsES, -strInstructionsFR, 
+         -strInstructionsIT, -`strInstructionsZH-HANS` , -`strInstructionsZH-HANT`, 
+         -strDrinkThumb, -strIngredient12, -strIngredient13, -strIngredient14, -strIngredient15, 
+         -strImageSource, -strImageAttribution, -strCreativeCommonsConfirmed, -dateModified,
+         -strMeasure12, -strMeasure13, -strMeasure14, -strMeasure15) %>% 
+  rename(cocktail_name = strDrink,
          category = strCategory,
          alcoholic = strAlcoholic,
          glass = strGlass,
@@ -40,6 +45,17 @@ all_cocktails = all_cocktails %>%
          ingredient_8 = strIngredient8,
          ingredient_9 = strIngredient9,
          ingredient_10 = strIngredient10,
-         ingredient_11 = strIngredient11)
+         ingredient_11 = strIngredient11,
+         measure_1 = strMeasure1,
+         measure_2 = strMeasure2,
+         measure_3 = strMeasure3,
+         measure_4 = strMeasure4,
+         measure_5 = strMeasure5,
+         measure_6 = strMeasure6,
+         measure_7 = strMeasure7,
+         measure_8 = strMeasure8,
+         measure_9 = strMeasure9,
+         measure_10 = strMeasure10,
+         measure_11 = strMeasure11)
 
 View(all_cocktails)
